6852cb9ea9b30a9ab25d0a33d1394a6b10e84b38
lim4349 <rockmanzero@naver.com>
2025-11-13 18:00:58 +0900

    feat: seq
    Signed-off-by: lim4349 <rockmanzero@naver.com>

--- vllm/model_executor/models/opencua_vl.py
+++ vllm/model_executor/models/opencua_vl.py
@@ -829,9 +829,8 @@ class OpenCUA_VisionTransformer(nn.Module):
     def get_rope_by_thw(self, t, h, w):
         window_index_thw, cu_seqlens_window_thw = self.get_window_index_thw(t, h, w)
         rotary_pos_emb_thw = self.rotary_pos_emb_thw(t, h, w)
-        window_index_groups = window_index_thw // self.spatial_merge_unit
-        window_index_groups = window_index_groups.to(rotary_pos_emb_thw.device)
-        rotary_pos_emb_thw = rotary_pos_emb_thw[window_index_groups, :, :]
+        window_index_thw = window_index_thw.to(rotary_pos_emb_thw.device)
+        rotary_pos_emb_thw = rotary_pos_emb_thw[window_index_thw, :, :]
         rotary_pos_emb_thw = rotary_pos_emb_thw.flatten(start_dim=0, end_dim=1)
         cu_seqlens_thw = torch.repeat_interleave(
             torch.tensor([h * w], dtype=torch.int32, device=rotary_pos_emb_thw.device),
